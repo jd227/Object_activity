a0f8619416b766b72d5ee8baf4695db12f29a38d
jd227 <jd821jd@gmail.com>
2024-08-02 17:09:46 -0500

    crea una propiedad a estudiantes

--- Object/Herencia/Estudiantes.js
+++ Object/Herencia/Estudiantes.js
@@ -1,5 +1,8 @@
 const Persona = require("../Encapsulamiento/Personas.js")
 
 class Estudiante extends Persona {
-
+    // crear un grado a la clase Estudiantes
+    constructor (Clase){
+        this.Clase = Clase;
+    }
 }
