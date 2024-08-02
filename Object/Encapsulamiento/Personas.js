859a18086fe1d36bb3a9b282ce20e03f9c36fcf0
jd227 <jd821jd@gmail.com>
2024-08-02 15:59:07 -0500

    Create_metodos_publicos

--- Object/Encapsulamiento/Personas.js
+++ Object/Encapsulamiento/Personas.js
@@ -1,11 +1,20 @@
-class Personas {
-    #nombre = '';
-    #edad = 0;
-    #fechaNacimiento= '';
+class Persona {
+    #nombre='';
+    #edad=0;
+    #fechaNacimiento='';
+  
+    constructor(nombre, edad, fechaNacimiento) {
+      this.establecerNombre(nombre);
+      this.establecerEdad(edad);
+      this.establecerFechaNacimiento(fechaNacimiento);
+    }
+  
 
-    constructor(nombre, edad, fechaNacimiento){
+    establecerNombre(nombre) {
+      if (typeof nombre === 'string' && nombre.trim() !== '') {
         this.#nombre = nombre;
-        this.#edad = edad;
-        this.#fechaNacimiento = fechaNacimiento;
+      } else {
+        throw new Error('Nombre no válido.');
+      }
     }
 }
