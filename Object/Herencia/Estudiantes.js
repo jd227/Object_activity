685c22319affa3251302197d6df723a01fb980ab
jd227 <jd821jd@gmail.com>
2024-08-02 18:39:55 -0500

    propiedad adicional para el grado del estudiante.

--- Object/Herencia/Estudiantes.js
+++ Object/Herencia/Estudiantes.js
@@ -1,8 +1,20 @@
 const Persona = require("../Encapsulamiento/Personas.js")
 
 class Estudiante extends Persona {
-    // crear un grado a la clase Estudiantes
-    constructor (Clase){
-        this.Clase = Clase;
+    grado;
+    constructor(nombre, edad, fechaNacimiento, gardo){
+        super(nombre, edad, fechaNacimiento);
+        this.grado = grado;
+    }
+    obtenerGrado(){
+        return this.grado;
+    }
+
+    establecerGrado(grado){
+        if(typeof grado === 'string' && grado.trim() !== ''){
+            this.grado = grado;
+        }else{
+            throw new Error('Grado invalido');
+        }
     }
 }
