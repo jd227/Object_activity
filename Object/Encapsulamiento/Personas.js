71b2a155510adafd487e913eca91f47ccd6d7d6d
jd227 <jd821jd@gmail.com>
2024-08-02 15:53:53 -0500

    Create_clase_personas

---
+++ Object/Encapsulamiento/Personas.js
@@ -0,0 +1,11 @@
+class Personas {
+    #nombre = '';
+    #edad = 0;
+    #fechaNacimiento= '';
+
+    constructor(nombre, edad, fechaNacimiento){
+        this.#nombre = nombre;
+        this.#edad = edad;
+        this.#fechaNacimiento = fechaNacimiento;
+    }
+}
