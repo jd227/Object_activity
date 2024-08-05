4e77bb27ac84f52aaaf9311c90eeac3916de7e71
jd227 <jd821jd@gmail.com>
2024-08-05 13:41:21 -0500

    Serie Numerica

---
+++ promesas/SerieNumerica.js
@@ -0,0 +1,129 @@
+// let variablesResultado = [];
+// let resultado = 1;
+// for (let i = 1; i <= 10; i++) {
+//     resultado = i + resultado;  
+//     console.log(resultado);
+// }
+
+// callback hell to promise...
+const numero = [];
+let N1 = 1;
+let N2 = 1;
+
+const nuevoNumero = () => new Promise(resolve => {
+    setTimeout(() => {
+        const suma = N1 + N2;
+        numero.push(suma);
+        resolve(`Agregado: ${suma}`);
+        N1 += 1;
+        N2 = suma; 
+    }, 100);
+});
+
+nuevoNumero()
+    .then(resultado => {
+        console.log(resultado);
+        console.log(numero);
+        return nuevoNumero();
+    })
+    .then(resultado => {
+        console.log(resultado);
+        console.log(numero);
+        return nuevoNumero();
+    })
+    .then(resultado => {
+        console.log(resultado);
+        console.log(numero);
+        return nuevoNumero();
+    })
+    .then(resultado => {
+        console.log(resultado);
+        console.log(numero);
+        return nuevoNumero();
+    })
+    .then(resultado => {
+        console.log(resultado);
+        console.log(numero);
+        return nuevoNumero();
+    })
+    .then(resultado => {
+        console.log(resultado);
+        console.log(numero);
+        return nuevoNumero();
+    })
+    .then(resultado => {
+        console.log(resultado);
+        console.log(numero);
+        return nuevoNumero();
+    })
+    .then(resultado => {
+        console.log(resultado);
+        console.log(numero);
+        return nuevoNumero();
+    })
+    .then(resultado => {
+        console.log(resultado);
+        console.log(numero);
+        return nuevoNumero();
+    })
+    .then(resultado => {
+        console.log(resultado);
+        console.log(numero);
+        return nuevoNumero();
+    })
+    .then(resultado => {
+        console.log(resultado);
+        console.log(numero);
+        return nuevoNumero();
+    })
+    .then(resultado => {
+        console.log(resultado);
+        console.log(numero);
+        return nuevoNumero();
+    })
+    .then(resultado => {
+        console.log(resultado);
+        console.log(numero);
+        return nuevoNumero();
+    })
+    .then(resultado => {
+        console.log(resultado);
+        console.log(numero);
+        return nuevoNumero();
+    })
+    .then(resultado => {
+        console.log(resultado);
+        console.log(numero);
+        return nuevoNumero();
+    })
+    .then(resultado => {
+        console.log(resultado);
+        console.log(numero);
+        return nuevoNumero();
+    })
+    .then(resultado => {
+        console.log(resultado);
+        console.log(numero);
+        return nuevoNumero();
+    })
+    .then(resultado => {
+        console.log(resultado);
+        console.log(numero);
+        return nuevoNumero();
+    })
+    .then(resultado => {
+        console.log(resultado);
+        console.log(numero);
+        return nuevoNumero();
+    })
+    .then(resultado => {
+        console.log(resultado);
+        console.log(numero);
+        return nuevoNumero();
+    })
+    .then(resultado => {
+        console.log(resultado);
+        console.log(numero);
+        return nuevoNumero();
+    });
+    
